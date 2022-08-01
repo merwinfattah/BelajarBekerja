5cda401dd3558205f1b72baf85b052b853b4220f
merwinfattah <18219019@std.stei.itb.ac.id>
2022-08-01 14:50:10 +0700

    initiation

---
+++ bb/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='mt-6 text-6xl bg-blue-800 mx-auto text-center'>BB</div>
+    </div>
+  );
+}
+
+export default App;
